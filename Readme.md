f9290c7308292c07ad5fa4bd8ad21df95c4b462e
Raghav Gupta <optimusprime0456@gmail.com>
2023-11-16 23:26:20 +0530

    Adding Files to github

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Portfolio Website | Web Page
+
+To show case my projects and skills
---
+++ index.js
@@ -0,0 +1,31 @@
+const navigateToHeading = document.querySelectorAll('.links').forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+
+            behavior: 'smooth'
+        });
+    })
+})
+
+const mailLink = document.querySelector('.mail').addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href = 'mailto:optimusprime0456@gmail.com'
+})
+const instagramLink = document.querySelector('.instagram').addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href = '#'
+})
+const telegramLink = document.querySelector('.telegram').addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href = '#'
+})
+const githubLink = document.querySelector('.github').addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href = '#'
+})
+const linkedinLink = document.querySelector('.linkedin').addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href = '#'
+})
+
