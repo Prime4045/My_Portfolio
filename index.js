9bfe854ad49b9afe8ba6dc4d913b716d9449569a
Raghav Gupta <optimusprime0456@gmail.com>
2023-11-19 23:08:58 +0530

    adding mobile responsiveness

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const navigateToHeading = document.querySelectorAll('.links').forEach(function (link) {
     link.addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,24 +9,33 @@ const navigateToHeading = document.querySelectorAll('.links').forEach(function (
     })
 })
 
-const mailLink = document.querySelector('.mail').addEventListener('click', (e) => {
-    e.preventDefault();
-    location.href = 'mailto:optimusprime0456@gmail.com'
-})
-const instagramLink = document.querySelector('.instagram').addEventListener('click', (e) => {
-    e.preventDefault();
-    location.href = '#'
-})
-const telegramLink = document.querySelector('.telegram').addEventListener('click', (e) => {
-    e.preventDefault();
-    location.href = '#'
-})
-const githubLink = document.querySelector('.github').addEventListener('click', (e) => {
-    e.preventDefault();
-    location.href = '#'
-})
-const linkedinLink = document.querySelector('.linkedin').addEventListener('click', (e) => {
-    e.preventDefault();
-    location.href = '#'
-})
+// const mailLink = document.querySelector('.mail').addEventListener('click', (e) => {
+//     e.preventDefault();
+//     location.href = 'mailto:optimusprime0456@gmail.com'
+// })
+// const instagramLink = document.querySelector('.instagram').addEventListener('click', (e) => {
+//     e.preventDefault();
+//     location.href = '#'
+// })
+// const telegramLink = document.querySelector('.telegram').addEventListener('click', (e) => {
+//     e.preventDefault();
+//     location.href = '#'
+// })
+// const githubLink = document.querySelector('.github').addEventListener('click', (e) => {
+//     e.preventDefault();
+//     location.href = '#'
+// })
+// const linkedinLink = document.querySelector('.linkedin').addEventListener('click', (e) => {
+//     e.preventDefault();
+//     location.href = '#'
+// })
+
+// const menu = document.querySelector(".nav_mobile");
+// const header = document.querySelector(".header_nav");
+
+// const toggleNavBar = () => {
+//     header.classList.toggle('active')
+// }
+
+// menu.addEventListener("click", () => toggleNavBar())
 
